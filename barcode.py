71828c5a65ff0a944a53b7176d523e2037b70460
SteinAcker1 <67439517+SteinAcker1@users.noreply.github.com>
2020-10-16 20:27:12 +0200

    Add files via upload

--- barcode.py
+++ barcode.py
@@ -39,16 +39,27 @@ undef = sys.argv[5]
 newFastq = []
 with open(fastq,'r') as f:
     for line in f:
+        #If the line marks the start of a header...
         if line.startswith('@'):
+            #Save that line and each of the next 3 lines as strings and add 
+            #them to the newFastq list as a tuple.
             sequence = next(f)
             plus = next(f)
             quality = next(f)
-            newFastq.append((line,sequence,plus,quality))
+            #This conditional eliminates the risk of the program finding an @
+            #used as a quality marker and mistaking it for a new header.
+            if plus == '+\n': 
+                newFastq.append((line,sequence,plus,quality))
             
 #%% Step 2: Separate these sequences into their respective FASTQ files.
 
 with open(sample1, 'w') as s1, open(sample2, 'w') as s2, open(sample3, 'w') as s3, open(undef, 'w') as u:
     for line in newFastq:
+        #For each piece of FASTQ data, the program checks if the barcode shows
+        #up at the start of the sequence (the 2nd part of each list) and, if 
+        #a barcode is found, the FASTQ data are sorted into the proper file.
+        #If no barcode is found, then the data are sorted into the 
+        #"undefined" file.
         if line[1].startswith('TATCCTCT'):
             s1.write(line[0]+line[1]+line[2]+line[3])
         elif line[1].startswith('GTAAGGAG'):
